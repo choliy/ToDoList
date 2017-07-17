2020a192220408dce50c64e2b10c90893716694e
Igor Choliy <choliy.igor@gmail.com>
2017-07-17 11:40:40 +0300

    Duplicated code removed

--- app/src/main/java/com/choliy/igor/todolist/util/MenuUtils.java
+++ app/src/main/java/com/choliy/igor/todolist/util/MenuUtils.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
 
-import com.choliy.igor.todolist.tool.ProjectConstants;
 import com.choliy.igor.todolist.R;
+import com.choliy.igor.todolist.tool.ProjectConstants;
 import com.yalantis.contextmenu.lib.ContextMenuDialogFragment;
 import com.yalantis.contextmenu.lib.MenuObject;
 import com.yalantis.contextmenu.lib.MenuParams;
@@ -22,33 +22,21 @@ public final class MenuUtils {
     public static ContextMenuDialogFragment setupMenu(Context context) {
         MenuObject closeMenu = new MenuObject();
         closeMenu.setResource(R.drawable.ic_close);
-        closeMenu.setDividerColor(R.color.colorPrimaryDark);
-        closeMenu.setBgResource(R.color.colorPrimary);
 
         MenuObject aboutMenu = new MenuObject(context.getString(R.string.menu_about));
         aboutMenu.setResource(R.drawable.ic_about);
-        aboutMenu.setDividerColor(R.color.colorPrimaryDark);
-        aboutMenu.setBgResource(R.color.colorPrimary);
 
         MenuObject shareMenu = new MenuObject(context.getString(R.string.menu_share));
         shareMenu.setResource(R.drawable.ic_share);
-        shareMenu.setDividerColor(R.color.colorPrimaryDark);
-        shareMenu.setBgResource(R.color.colorPrimary);
 
         MenuObject emailMenu = new MenuObject(context.getString(R.string.menu_email));
         emailMenu.setResource(R.drawable.ic_email);
-        emailMenu.setDividerColor(R.color.colorPrimaryDark);
-        emailMenu.setBgResource(R.color.colorPrimary);
 
         MenuObject feedbackMenu = new MenuObject(context.getString(R.string.menu_feedback));
         feedbackMenu.setResource(R.drawable.ic_feedback);
-        feedbackMenu.setDividerColor(R.color.colorPrimaryDark);
-        feedbackMenu.setBgResource(R.color.colorPrimary);
 
         MenuObject appsMenu = new MenuObject(context.getString(R.string.menu_more_apps));
         appsMenu.setResource(R.drawable.ic_apps);
-        appsMenu.setDividerColor(R.color.colorPrimaryDark);
-        appsMenu.setBgResource(R.color.colorPrimary);
 
         List<MenuObject> menuObjects = new ArrayList<>();
         menuObjects.add(closeMenu);
@@ -57,6 +45,11 @@ public final class MenuUtils {
         menuObjects.add(emailMenu);
         menuObjects.add(feedbackMenu);
         menuObjects.add(appsMenu);
+
+        for (MenuObject menuObject : menuObjects) {
+            menuObject.setDividerColor(R.color.colorPrimaryDark);
+            menuObject.setBgResource(R.color.colorPrimary);
+        }
 
         MenuParams menuParams = new MenuParams();
         menuParams.setActionBarSize(getActionBarHeight(context));
